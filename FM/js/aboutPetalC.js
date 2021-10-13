cc948ebb88c7313ab4e104decbee998510295d98
jingshing001 <a2324395@gmail.ocm>
2021-10-13 11:37:29 +0800

    add

--- FM/js/aboutPetalC.js
+++ FM/js/aboutPetalC.js
@@ -7,9 +7,9 @@ let petal4=document.getElementById("petal4")
 checkbox.addEventListener('change', () => {
 
     if (changeColor.classList.contains("dark")) {
-      petal4.src="../images/abu-flower12.png"
+      petal4.src="images/abu-flower12.png"
       
     } else {
-        petal4.src="../images/abu-flower02.png"
+        petal4.src="images/abu-flower02.png"
     }
 })
