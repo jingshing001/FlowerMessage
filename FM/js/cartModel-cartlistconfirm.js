1986918e3883ad916ce6accf6738ecdeecb117a5
jingshing001 <a2324395@gmail.ocm>
2021-10-14 17:08:32 +0800

    add

--- FM/js/cartModel-cartlistconfirm.js
+++ FM/js/cartModel-cartlistconfirm.js
@@ -4,73 +4,99 @@ let result01 = document.getElementById('result01');
 let result02 = document.getElementById('result02');
 let result03 = document.getElementById('result03');
 
-function thousandBitSeparator(num) {
-    return num && (num.toString().indexOf('.') != -1 ? num.toString().replace(/(\d)(?=(\d{3})+\.)/g, function ($0, $1) {
-        return $1 + ",";
-    }) : num.toString().replace(/(\d)(?=(\d{3}))/g, function ($0, $1) {
-        return $1 + ",";
-    }));
+let tutal=product01.value*480+product02.value*680
+
+
+result01.innerHTML=product01.value*480;
+result02.innerHTML=product02.value*680;
+result03.innerHTML=Thousands(tutal);
+
+//千分位
+function Thousands(num){
+    let newNum= String(num).split('').reverse().reduce((p,n,index)=>{
+            return index%3 ? n+''+p : n+','+p; 
+     });
+     return newNum;
 }
 
-result01.innerHTML = thousandBitSeparator(product01.value * 480);
-result02.innerHTML = thousandBitSeparator(product02.value * 680);
-result03.innerHTML = thousandBitSeparator(product01.value * 480 + product02.value * 680);
+//input數量監聽
+product01.addEventListener('change',function(){
+        tutal=product01.value*480+product02.value*680
+        result01.innerHTML=Thousands(product01.value*480);
+        result03.innerHTML=Thousands(tutal)
+})
 
-product01.addEventListener('change', function () {
-    result01.innerHTML = thousandBitSeparator(product01.value * 480);
-    result03.innerHTML = thousandBitSeparator(product01.value * 480 + product02.value * 680);
+//input數量監聽
+product02.addEventListener('change',function(){
+        tutal=product01.value*480+product02.value*680
+        result02.innerHTML=Thousands(product02.value*680);
+        result03.innerHTML=Thousands(tutal);
 })
-product02.addEventListener('change', function () {
-    result02.innerHTML = thousandBitSeparator(product02.value * 680);
-    result03.innerHTML = thousandBitSeparator(product01.value * 480 + product02.value * 680);
-})
+
+
+
+
+
+
+
 
 
 // 刪除按鈕
-
 let deleteBtn = document.getElementsByClassName('deleteBtn');
 let deleteAlert = document.getElementById('deleteAlert');
 let allContent = document.getElementById('allContent');
 let retu = document.getElementById('retu');
 let yes = document.getElementById('yes');
 let mask = document.getElementById('mask');
-let cartList = document.querySelectorAll('#cartlistconfirm table tr');
+let deleteBtnArray=[...deleteBtn];
 
-for (i = 0; i <= 1; i++) {
-    (function (x) {
-        deleteBtn[x].addEventListener('click', () => {
-            deleteAlert.classList.remove('hidden');
-            allContent.classList.add('blur');
-            mask.classList.add('mask');
 
-            yes.addEventListener('click', () => {
-                deleteAlert.classList.add('hidden');
-                allContent.classList.remove('blur');
-                mask.classList.remove('mask');
-                cartList[x + 1].remove();
 
-                switch (x) {
-                    case 0:
-                        result03.innerHTML = thousandBitSeparator(product02.value * 680);
-                        break;
-                    case 1:
-                        result03.innerHTML = thousandBitSeparator(product01.value * 480);
-                        break;
-                }
-            })
-        })
-
-    })(i)
+//移除遮罩
+function removeMask(){
+    deleteAlert.classList.add('hidden');
+    allContent.classList.remove('blur');
+    mask.classList.remove('mask');
 }
 
-retu.addEventListener('click', () => {
-    deleteAlert.classList.add('hidden');
-    allContent.classList.remove('blur');
-    mask.classList.remove('mask');
+//增加遮罩
+function addMask(){
+    deleteAlert.classList.remove('hidden');
+    allContent.classList.add('blur');
+    mask.classList.add('mask');
+}
+
+
+//移除表格
+function deleteList(){
+    yes.addEventListener('click',()=>{
+        this.parentElement.parentElement.remove();
+        removeMask();
+        console.dir(this);
+})
+}
+
+//刪除圖示監聽
+deleteBtnArray.forEach((v,index)=>{
+    v.addEventListener('click',function(){
+        addMask();
+        deleteList.call(v);
+        console.log(index);
+        if(index==0){
+            tutal=tutal-product01.value*480
+            result03.innerHTML=tutal
+        }else{
+            tutal=tutal-product02.value*680
+            result03.innerHTML=tutal
+        }
+    })
 })
 
-mask.addEventListener('click', () => {
-    deleteAlert.classList.add('hidden');
-    allContent.classList.remove('blur');
-    mask.classList.remove('mask');
+
+retu.addEventListener('click',function(){
+    removeMask();
+})
+
+mask.addEventListener('click',function(){
+    removeMask();
 })
